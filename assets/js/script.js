18ee162d26fc6974b6b775b258a927c2a5966187
Dylan Shoemaker <me@dylanshoemaker.com>
2021-07-04 03:28:36 -0600

    create if, else if, else statement for password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 
 //need to declare confirms for user input booleans
-var confirmUpperCase
-var confirmLowerCase
-var confirmNumbers
-var confirmSymbols 
+var confirmUpperCase;
+var confirmLowerCase;
+var confirmNumbers;
+var confirmSymbols;
 
 // for password length
 var userInputLength = [];
@@ -14,39 +14,44 @@ var lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
 var numbers = ["0123456789"]
 var symbols = ["!#$%&()*+-:;<=>?@[\]^_`{|}~"]
 
-// Get references to the #generate element
+// Get references to the #generate element in html with id = generate
 var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", displayPassword);
 
 //start password generator by user pressing button
+var passwordMachineGoBrrr = function() {
+  //prompt What length would you like your new password to be? You can pick any length between 8 and 128 characters!
+  var confirmNumbers = parseInt.prompt("What length would you like your new password to be? You can pick any length between 8 and 128 characters!");
+  //this is for no response
+  if (!userInputLength) {
+    alert("Have you ever seen a blank password before? Neither have we. Please try again.");
+    return " ";
+    //response is not between 8-128 characters 
+  } else if (userInputLength < 8 || userInputLength > 128) {
+    userInputLength = alert("You absolutely must enter a number between 8 and 128, I insist!");
+    return " ";
+  }
+  else {
+    var confirmUpperCase = confirm("Would you like lower case letters? e.g. abcde");
+    var confirmLowerCase = confirm("Would you like UPPER CASE letters? e.g. ABCDE");
+    var confirmNumbers = confirm("Would you like Numbers? e.g. 1337");
+    var confirmSymbols = confirm("Would you like special characters? e.g. $#!@");
+  }  
+      
 
 
-//prompt What length would you like your new password to be? You can pick any length between 8 and 128 characters!
+}
 
-//if 
-//response is not between 8-128 characters 
-//then
-//alert Invalid response, password length must be 8-128 characters long.
-//break;
-
-//window.confirm Would you like lower case letters? e.g. abcde
-
-//window.confirm Would you like include UPPER CASE letters? e.g. ABCDE
-
-//window.confirm Would you like Numbers? e.g. 1337
-
-//window.confirm Would you like special characters? e.g. $#!@
-
-// if 
+// else 
 //false for all four options 
 //then
 //alert Invalid response, password must have at least one type of character.
 //break;
 
 // Write password to the #password input
-function writePassword() {
+function displayPassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
