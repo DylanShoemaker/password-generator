4e2ebf302677881a5660d5886872bfdfa36d686e
Dylan Shoemaker <me@dylanshoemaker.com>
2021-07-04 05:06:35 -0600

    clean up code, adjust values, finish comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,8 @@
-
-//need to declare confirms for user input booleans
-var confirmUpperCase;
-var confirmLowerCase;
-var confirmNumbers;
-var confirmSymbols;
-
-// for password length
-var userInputLength = [];
-
 //put var numbers letters and symbols here
-var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
-var numbers = ["0123456789"]
-var symbols = ["!#$%&()*+-:;<=>?@[\]^_`{|}~"]
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var numbers = ["0123456789"];
+var symbols = ["!#$%&()*+-:;<=>?@[\]^_`{|}~"];
 
 // Get references to the #generate element in html with id = generate
 var generateBtn = document.querySelector("#generate");
@@ -21,49 +11,62 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", displayPassword);
 
 //start password generator by user pressing button
-var passwordMachineGoBrrr = function() {
+function passwordMachineGoBrrr() {
   //prompt What length would you like your new password to be? You can pick any length between 8 and 128 characters!
-  var confirmNumbers = parseInt.prompt("What length would you like your new password to be? You can pick any length between 8 and 128 characters!");
+  var userInputLength = parseInt(prompt("What length would you like your new password to be? You can pick any length between 8 and 128 characters!"));
   //this is for no response
   if (!userInputLength) {
     alert("Have you ever seen a blank password before? Neither have we. Please try again.");
-    return " ";
+    return "";
     //response is not between 8-128 characters 
   } else if (userInputLength < 8 || userInputLength > 128) {
     userInputLength = alert("You absolutely must enter a number between 8 and 128, I insist!");
-    return " ";
-  }
-  else {
-    var confirmUpperCase = confirm("Would you like lower case letters? e.g. abcde");
-    var confirmLowerCase = confirm("Would you like UPPER CASE letters? e.g. ABCDE");
+    return "";
+  } else {
+    var confirmLowerCase = confirm("Would you like lower case letters? e.g. abcde");
+    var confirmUpperCase = confirm("Would you like UPPER CASE letters? e.g. ABCDE");
     var confirmNumbers = confirm("Would you like Numbers? e.g. 1337");
     var confirmSymbols = confirm("Would you like special characters? e.g. $#!@");
   }  
-      
+ 
+  if (!confirmUpperCase && !confirmLowerCase && !confirmNumbers && !confirmSymbols) {
+    alert("Invalid response, password must have at least one type of character, please choose at least one option.");
+    return "";
+  }
 
+  //use concatenation.....example from w3: string.concat(string1, string2, ..., stringX)  
+  //if you make the string "" ( not null ) then you can use this to simply get all the values...
+  //...of the 4 categories and then you just run a 4 loop to put it all together in the return at the bottom of the entire function
+  //... after that you need to 
+  var charCodes = "";
+ 
+  if (confirmUpperCase) {
+    charCodes = charCodes.concat(upperCase);
+  }
+  if (confirmLowerCase) {
+    charCodes = charCodes.concat(lowerCase);
+  }
+  if (confirmNumbers) {
+    charCodes = charCodes.concat(numbers);
+  }
+  if (confirmSymbols) {
+    charCodes = charCodes.concat(symbols);
+  }
+  
+  // this is the coding version of a junk drawer where a bunch of random things will find its home
+  var finalProduct = [];
 
-}
+  // this is where everything is actually compiled
+  for (var i = 0; i < userInputLength; i++) {
+    finalProduct = finalProduct + charCodes[Math.floor(Math.random() * charCodes.length)];
+  }
 
-// else 
-//false for all four options 
-//then
-//alert Invalid response, password must have at least one type of character.
-//break;
+  return finalProduct; 
+};
 
-// Write password to the #password input
-function displayPassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  //var upperCase 
-  //var lowerCase 
-  //var numbers 
-  //var symbols 
-
-  passwordText.value = password;
-
-}
-
-
-
-// password appears in the form of an alert in textarea
+// links to html
+function displayPassword () {
+      var password = passwordMachineGoBrrr();
+      var passwordText = document.getElementById("password");
+      passwordText.value = password;
+};
